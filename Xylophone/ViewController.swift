e3a8c5122834b1f6174b64ebf14971d4a38f1d30
Zubair Irfan Khan <zubair.khan@airlifttech.com>
2022-07-16 14:45:33 +0500

    uploading to github

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        setOpacity(button: sender, alpha: 0.5)
+        playSound(soundToPlay: sender.currentTitle!)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+          //Brings sender's opacity back up to fully opaque.
+            self.setOpacity(button: sender, alpha: 1)
+        }
 
+    }
     
-
+    func playSound(soundToPlay: String) {
+        let url = Bundle.main.url(forResource: soundToPlay, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+    
+    func setOpacity(button: UIButton, alpha: Float) {
+        button.alpha = CGFloat(alpha)
+    }
 }
+
+
+
 
